7d384c031fc9bc390bce4e4b4e1cb7c67dac9f34
Guru Gali <pranavgali@icloud.com>
2026-09-26 12:57:57 -0400

    debugging and fixed code

--- HelpingHandCode/src/mainCode.cpp
+++ HelpingHandCode/src/mainCode.cpp
@@ -7,30 +7,40 @@ Servo myServo;
 
 int buttonPin = 8;
 int servoPin = 3;
-volatile boolean ButtonToggle = false;
+boolean ButtonToggle = false;
+boolean LastButtonState = HIGH;
+
 
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(9600);
 pinMode(buttonPin, INPUT_PULLUP);
 myServo.attach(servoPin);
 myServo.write(90);
+Serial.println("Starting");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int ButtonState = digitalRead(buttonPin);
 
-  if (ButtonState == HIGH){
-    ButtonToggle != ButtonToggle;
+  if (ButtonState == LOW && LastButtonState == HIGH){
+    ButtonToggle = !ButtonToggle;
+    Serial.println("Button Pressed");
   }
 
   if (ButtonToggle == true){
-    myServo.write(0); }
+    myServo.write(0);
+  Serial.println("Servo to 0"); }
 
   else if (ButtonToggle == false){
     myServo.write(90);
-  }
+    Serial.println("Servo to 90");
   }
 
+  delay(50);
+  LastButtonState = ButtonState;
+  Serial.println(LastButtonState);
+  }
 
 // put function definitions here
